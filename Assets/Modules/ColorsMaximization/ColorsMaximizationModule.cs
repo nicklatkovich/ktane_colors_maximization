43fed7de3eaf1e5d643241bfaa54bbd2c411f95e
NickLatkovich <nicklatkovich@gmail.com>
2021-04-01 00:36:49 +0300

    Fix max score calculations && Add logging

--- Assets/Modules/ColorsMaximization/ColorsMaximizationModule.cs
+++ Assets/Modules/ColorsMaximization/ColorsMaximizationModule.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using KModkit;
 
 public class ColorsMaximizationModule : MonoBehaviour {
+	private static int _moduleIdCounter = 1;
+
 	public ButtonComponent buttonPrefab;
 	public KMSelectable submitButton;
 	public GameObject buttonsCollection;
@@ -11,6 +13,7 @@ public class ColorsMaximizationModule : MonoBehaviour {
 	public KMBombInfo bomb;
 
 	private bool _passed = false;
+	private int _moduleId;
 	private readonly Color[] _allColors = {
 		Color.red,
 		Color.green,
@@ -19,22 +22,42 @@ public class ColorsMaximizationModule : MonoBehaviour {
 		Color.yellow,
 		Color.white,
 	};
+	private readonly Color[][] _rules = {
+		new Color[] { Color.red, Color.green, Color.blue, Color.magenta, Color.yellow, Color.white },
+		new Color[] { Color.green, Color.red, Color.yellow, Color.magenta, Color.blue, Color.white },
+		new Color[] { Color.white, Color.blue, Color.red, Color.green, Color.magenta, Color.yellow },
+		new Color[] { Color.magenta, Color.green, Color.blue, Color.red, Color.white, Color.yellow },
+		new Color[] { Color.magenta, Color.yellow, Color.white, Color.green, Color.red, Color.blue },
+		new Color[] { Color.white, Color.yellow, Color.magenta, Color.blue, Color.green, Color.red },
+	};
 	private List<ButtonComponent> _buttons = new List<ButtonComponent>();
 	private Dictionary<Color, int> _countOfColor = new Dictionary<Color, int>();
+	private Dictionary<Color, string> _colorPrettier = new Dictionary<Color, string> {
+		{ Color.red, "Red" },
+		{ Color.green, "Green" },
+		{ Color.blue, "Blue" },
+		{ Color.magenta, "Magenta" },
+		{ Color.yellow, "Yellow" },
+		{ Color.white, "White" },
+	};
 
 	public Color[] GetOrderedColors() {
-		if (bomb.GetBatteryCount() == 4) {
-			return new Color[] { Color.red, Color.green, Color.blue, Color.magenta, Color.yellow, Color.white };
-		} else if (bomb.GetOffIndicators().Count() == 2) {
-			return new Color[] { Color.green, Color.red, Color.yellow, Color.magenta, Color.blue, Color.white };
-		} else if (bomb.GetSerialNumberNumbers().Select(e => e % 2 == 0).Count() == 2) {
-			return new Color[] { Color.white, Color.blue, Color.red, Color.green, Color.magenta, Color.yellow };
-		} else if (bomb.IsPortPresent(Port.Serial)) {
-			return new Color[] { Color.magenta, Color.green, Color.blue, Color.red, Color.white, Color.yellow };
-		} else if (bomb.GetTwoFactorCodes().Any(e => e % 3 == 0 || e % 5 == 0)) {
-			return new Color[] { Color.magenta, Color.yellow, Color.white, Color.green, Color.red, Color.blue };
+		if (bomb.GetBatteryCount() == 4) return useRule(1);
+		else if (bomb.GetOffIndicators().Count() == 2) return useRule(2);
+		else if (bomb.GetSerialNumberNumbers().Where(e => e % 2 == 0).Count() == 2) return useRule(3);
+		else if (bomb.IsPortPresent(Port.Serial)) return useRule(4);
+		else if (bomb.GetTwoFactorCodes().Any(e => e % 3 == 0 || e % 5 == 0)) return useRule(5);
+		return useRule(6);
+	}
+
+	private Color[] useRule(int ruleNumber) {
+		Debug.LogFormat("[ColorsMaximization #{0}] Using rule #{1}", _moduleId, ruleNumber);
+		Color[] result = _rules[ruleNumber - 1];
+		for (int i = 0; i < result.Length; i++) {
+			string prettier = _colorPrettier[result[i]];
+			Debug.LogFormat("[ColorsMaximization #{0}] {1} color score = {2}", _moduleId, prettier, i + 1);
 		}
-		return new Color[] { Color.white, Color.yellow, Color.magenta, Color.blue, Color.green, Color.red };
+		return result;
 	}
 
 	private HashSet<Color> getHalfRandomColors() {
@@ -52,6 +75,7 @@ public class ColorsMaximizationModule : MonoBehaviour {
 	}
 
 	private void Start() {
+		_moduleId = _moduleIdCounter++;
 		KMSelectable selfSelectable = GetComponent<KMSelectable>();
 		foreach (Color color in _allColors) _countOfColor[color] = 0;
 		List<KMSelectable> children = new List<KMSelectable>();
@@ -74,6 +98,11 @@ public class ColorsMaximizationModule : MonoBehaviour {
 				children.Add(selectableButton);
 			}
 		}
+		foreach (Color color in _allColors) {
+			string prettier = _colorPrettier[color];
+			int count = _countOfColor[color];
+			Debug.LogFormat("[ColorsMaximization #{0}] {1} color count = {2}", _moduleId, prettier, count);
+		}
 		children.Add(submitButton);
 		selfSelectable.Children = children.ToArray();
 		selfSelectable.UpdateChildren();
@@ -94,27 +123,34 @@ public class ColorsMaximizationModule : MonoBehaviour {
 	private bool OnSubmitPressed() {
 		if (_passed) return false;
 		Color[] colors = GetOrderedColors();
-		int prevPrevScore = 0;
-		int prevScore = 0;
 		Dictionary<Color, int> scoreOfColor = new Dictionary<Color, int>();
-		for (int score = 1; score <= colors.Length; score++) {
-			Color color = colors[score - 1];
+		int[] scores = new int[6];
+		for (int i = 0; i < colors.Length; i++) {
+			int score = i + 1;
+			Color color = colors[i];
 			scoreOfColor[color] = score;
-			int newScore = Mathf.Max(prevPrevScore + score * _countOfColor[color], prevScore);
-			prevPrevScore = prevScore;
-			prevScore = newScore;
+			int totalColorScore = score * _countOfColor[color];
+			scores[i] = totalColorScore;
+			if (i > 1) scores[i] += scores[i - 2];
+			if (i > 0) scores[i] = Mathf.Max(scores[i], scores[i - 1]);
 		}
-		int expectedScore = prevScore;
+		int expectedScore = Mathf.Max(scores[colors.Length - 1], scores[colors.Length - 2]);
+		Debug.LogFormat("[ColorsMaximization #{0}] Expected score: {1}", _moduleId, expectedScore);
 		int submitedScore = 0;
 		foreach (ButtonComponent button in _buttons) {
 			if (!button.active) continue;
 			submitedScore += scoreOfColor[button.primaryColor];
 		}
+		Debug.LogFormat("[ColorsMaximization #{0}] Submited score: {1}", _moduleId, submitedScore);
 		if (submitedScore == expectedScore) {
 			_passed = true;
 			GetComponent<KMBombModule>().HandlePass();
 		} else GetComponent<KMBombModule>().HandleStrike();
 		KMAudio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, this.transform);
 		return false;
+	}
+
+	private void LogRule(int ruleNumber) {
+		Debug.LogFormat("[ColorsMaximization #{0}] Using rule #{1}", _moduleId, ruleNumber);
 	}
 }
