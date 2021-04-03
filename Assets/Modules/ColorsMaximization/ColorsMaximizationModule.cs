c9265c63e77a24f8ec914d3b3578299b692272d5
NickLatkovich <nicklatkovich@gmail.com>
2021-04-03 22:43:55 +0300

    Add Twitch Plays support

--- Assets/Modules/ColorsMaximization/ColorsMaximizationModule.cs
+++ Assets/Modules/ColorsMaximization/ColorsMaximizationModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Text.RegularExpressions;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using KModkit;
@@ -11,6 +12,12 @@ public class ColorsMaximizationModule : MonoBehaviour {
 	public GameObject buttonsCollection;
 	public KMAudio KMAudio;
 	public KMBombInfo bomb;
+
+	public const int WIDTH = 5;
+	public const int HEIGHT = 4;
+	public const string TwitchHelpMessage = (
+		"`!{0} a1 2;2 15 submit` to press buttons by coordinates, number in reading order or button with label `submit`"
+	);
 
 	private bool _passed = false;
 	private int _moduleId;
@@ -31,6 +38,7 @@ public class ColorsMaximizationModule : MonoBehaviour {
 		new Color[] { Color.white, Color.yellow, Color.magenta, Color.blue, Color.green, Color.red },
 	};
 	private List<ButtonComponent> _buttons = new List<ButtonComponent>();
+	private ButtonComponent[][] _buttonsGrid = new ButtonComponent[0][] { };
 	private Dictionary<Color, int> _countOfColor = new Dictionary<Color, int>();
 	private Dictionary<Color, string> _colorPrettier = new Dictionary<Color, string> {
 		{ Color.red, "Red" },
@@ -41,50 +49,19 @@ public class ColorsMaximizationModule : MonoBehaviour {
 		{ Color.white, "White" },
 	};
 
-	public Color[] GetOrderedColors() {
-		if (bomb.GetBatteryCount() == 4) return useRule(1);
-		else if (bomb.GetOffIndicators().Count() == 2) return useRule(2);
-		else if (bomb.GetSerialNumberNumbers().Where(e => e % 2 == 0).Count() == 2) return useRule(3);
-		else if (bomb.IsPortPresent(Port.Serial)) return useRule(4);
-		else if (bomb.GetTwoFactorCodes().Any(e => e % 3 == 0 || e % 5 == 0)) return useRule(5);
-		return useRule(6);
-	}
-
-	private Color[] useRule(int ruleNumber) {
-		Debug.LogFormat("[ColorsMaximization #{0}] Using rule #{1}", _moduleId, ruleNumber);
-		Color[] result = _rules[ruleNumber - 1];
-		for (int i = 0; i < result.Length; i++) {
-			string prettier = _colorPrettier[result[i]];
-			Debug.LogFormat("[ColorsMaximization #{0}] {1} color score = {2}", _moduleId, prettier, i + 1);
-		}
-		return result;
-	}
-
-	private HashSet<Color> getHalfRandomColors() {
-		HashSet<Color> result = new HashSet<Color>();
-		Color[] colors = _allColors.Clone() as Color[];
-		for (int i = 0; i < colors.Length; i++) {
-			int pos = Random.Range(0, colors.Length - 1);
-			Color tmp = colors[i];
-			colors[i] = colors[pos];
-			colors[pos] = tmp;
-		}
-		int size = Random.Range(colors.Length / 2, colors.Length / 2 + 1 + (colors.Length % 2 == 0 ? 0 : 1));
-		for (int i = 0; i < size; i++) result.Add(colors[i]);
-		return result;
-	}
-
 	private void Start() {
 		_moduleId = _moduleIdCounter++;
 		KMSelectable selfSelectable = GetComponent<KMSelectable>();
 		foreach (Color color in _allColors) _countOfColor[color] = 0;
 		List<KMSelectable> children = new List<KMSelectable>();
 		HashSet<Color> colorsActiveAtStart = getHalfRandomColors();
-		for (int x = 0; x < 5; x++) {
-			for (int z = 0; z < 4; z++) {
+		_buttonsGrid = new ButtonComponent[WIDTH][];
+		for (int x = 0; x < WIDTH; x++) {
+			_buttonsGrid[x] = new ButtonComponent[HEIGHT];
+			for (int z = 0; z < HEIGHT; z++) {
 				ButtonComponent button = Instantiate(buttonPrefab);
 				button.transform.parent = buttonsCollection.transform;
-				button.transform.localPosition = new Vector3(x * 0.02f, 0.015f, z * 0.02f);
+				button.transform.localPosition = new Vector3(x * 0.02f, 0.015f, (HEIGHT - z - 1) * 0.02f);
 				button.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
 				button.transform.localEulerAngles = Vector3.zero;
 				Color color = _allColors[Random.Range(0, _allColors.Length)];
@@ -96,12 +73,13 @@ public class ColorsMaximizationModule : MonoBehaviour {
 				selectableButton.OnInteract += () => OnButtonPressed(button);
 				selectableButton.Parent = selfSelectable;
 				children.Add(selectableButton);
+				_buttonsGrid[x][z] = button;
 			}
 		}
 		foreach (Color color in _allColors) {
 			string prettier = _colorPrettier[color];
 			int count = _countOfColor[color];
-			Debug.LogFormat("[ColorsMaximization #{0}] {1} color count = {2}", _moduleId, prettier, count);
+			Debug.LogFormat("[Colors Maximization #{0}] {1} color count = {2}", _moduleId, prettier, count);
 		}
 		children.Add(submitButton);
 		selfSelectable.Children = children.ToArray();
@@ -120,6 +98,78 @@ public class ColorsMaximizationModule : MonoBehaviour {
 		return false;
 	}
 
+	public KMSelectable[] ProcessTwitchCommand(string command) {
+		command = command.Trim().ToLower();
+		KMSelectable[] parsedCoords = parseButtonsSet(command);
+		if (parsedCoords != null) return parsedCoords;
+		return null;
+	}
+
+	public Color[] GetOrderedColors() {
+		if (bomb.GetBatteryCount() == 4) return useRule(1);
+		else if (bomb.GetOffIndicators().Count() == 2) return useRule(2);
+		else if (bomb.GetSerialNumberNumbers().Where(e => e % 2 == 0).Count() == 2) return useRule(3);
+		else if (bomb.IsPortPresent(Port.Serial)) return useRule(4);
+		else if (bomb.GetTwoFactorCodes().Any(e => e % 3 == 0 || e % 5 == 0)) return useRule(5);
+		return useRule(6);
+	}
+
+	public Color[] useRule(int ruleNumber) {
+		Debug.LogFormat("[Colors Maximization #{0}] Using rule #{1}", _moduleId, ruleNumber);
+		Color[] result = _rules[ruleNumber - 1];
+		for (int i = 0; i < result.Length; i++) {
+			string prettier = _colorPrettier[result[i]];
+			Debug.LogFormat("[Colors Maximization #{0}] {1} color score = {2}", _moduleId, prettier, i + 1);
+		}
+		return result;
+	}
+
+	public HashSet<Color> getHalfRandomColors() {
+		HashSet<Color> result = new HashSet<Color>();
+		Color[] colors = _allColors.Clone() as Color[];
+		for (int i = 0; i < colors.Length; i++) {
+			int pos = Random.Range(0, colors.Length - 1);
+			Color tmp = colors[i];
+			colors[i] = colors[pos];
+			colors[pos] = tmp;
+		}
+		int size = Random.Range(colors.Length / 2, colors.Length / 2 + 1 + (colors.Length % 2 == 0 ? 0 : 1));
+		for (int i = 0; i < size; i++) result.Add(colors[i]);
+		return result;
+	}
+
+	public KMSelectable[] parseButtonsSet(string set) {
+		string[] strings = set.Split(' ').Where(r => r.Length > 0).ToArray();
+		KMSelectable[] result = new KMSelectable[strings.Count()];
+		for (int i = 0; i < strings.Count(); i++) {
+			KMSelectable selectable = parseButton(strings[i]);
+			if (selectable == null) return null;
+			result[i] = selectable;
+		}
+		return result;
+	}
+
+	public KMSelectable parseButton(string str) {
+		if (str == "submit") return submitButton;
+		Match match = Regex.Match(str, @"([a-e])([1-4])");
+		if (match.Success) {
+			int column = match.Groups[1].Value[0] - 'a';
+			return _buttonsGrid[column][int.Parse(match.Groups[2].Value) - 1].GetComponent<KMSelectable>();
+		}
+		match = Regex.Match(str, @"([1-5])[;,]([1-4])");
+		if (match.Success) {
+			int column = int.Parse(match.Groups[1].Value) - 1;
+			return _buttonsGrid[column][int.Parse(match.Groups[2].Value) - 1].GetComponent<KMSelectable>();
+		}
+		if (Regex.IsMatch(str, @"0|[1-9]\d*")) {
+			int index = int.Parse(str) - 1;
+			int row = index / WIDTH;
+			if (row >= HEIGHT) return null;
+			return _buttonsGrid[index % WIDTH][row].GetComponent<KMSelectable>();
+		}
+		return null;
+	}
+
 	private bool OnSubmitPressed() {
 		if (_passed) return false;
 		Color[] colors = GetOrderedColors();
@@ -135,13 +185,13 @@ public class ColorsMaximizationModule : MonoBehaviour {
 			if (i > 0) scores[i] = Mathf.Max(scores[i], scores[i - 1]);
 		}
 		int expectedScore = Mathf.Max(scores[colors.Length - 1], scores[colors.Length - 2]);
-		Debug.LogFormat("[ColorsMaximization #{0}] Expected score: {1}", _moduleId, expectedScore);
+		Debug.LogFormat("[Colors Maximization #{0}] Expected score: {1}", _moduleId, expectedScore);
 		int submitedScore = 0;
 		foreach (ButtonComponent button in _buttons) {
 			if (!button.active) continue;
 			submitedScore += scoreOfColor[button.primaryColor];
 		}
-		Debug.LogFormat("[ColorsMaximization #{0}] Submited score: {1}", _moduleId, submitedScore);
+		Debug.LogFormat("[Colors Maximization #{0}] Submited score: {1}", _moduleId, submitedScore);
 		if (submitedScore == expectedScore) {
 			_passed = true;
 			GetComponent<KMBombModule>().HandlePass();
@@ -151,6 +201,6 @@ public class ColorsMaximizationModule : MonoBehaviour {
 	}
 
 	private void LogRule(int ruleNumber) {
-		Debug.LogFormat("[ColorsMaximization #{0}] Using rule #{1}", _moduleId, ruleNumber);
+		Debug.LogFormat("[Colors Maximization #{0}] Using rule #{1}", _moduleId, ruleNumber);
 	}
 }
