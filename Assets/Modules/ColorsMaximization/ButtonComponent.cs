cec4a12c2cafba545c47e4fb6553971ecfa483cc
NickLatkovich <nicklatkovich@gmail.com>
2021-04-04 22:59:15 +0300

    Add custom highlight

--- Assets/Modules/ColorsMaximization/ButtonComponent.cs
+++ Assets/Modules/ColorsMaximization/ButtonComponent.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 
 public class ButtonComponent : MonoBehaviour {
+	public static readonly Color DEFAULT_BUTTON_COLOR = new Color(.33f, .33f, .33f);
+
 	public TextMesh textMesh;
 	public Renderer glass;
+	public Renderer button;
+	public Shader selectedShader;
+	public Shader unselectedShader;
 
 	private bool _colorblindMode = false;
 	public bool colorblindMode {
@@ -34,12 +39,38 @@ public class ButtonComponent : MonoBehaviour {
 		}
 	}
 
+	private bool selected = false;
+
+	private void Start() {
+		KMSelectable selfSelectable = GetComponent<KMSelectable>();
+		selfSelectable.OnHighlight += () => {
+			selected = true;
+			button.material.shader = selectedShader;
+			highlight();
+		};
+		selfSelectable.OnHighlightEnded += () => {
+			selected = false;
+			button.material.shader = unselectedShader;
+			button.material.SetColor("_Color", DEFAULT_BUTTON_COLOR);
+		};
+	}
+
 	private void OnChanged() {
 		if (!_colorblindMode || !_active) textMesh.text = "";
 		else {
 			textMesh.text = ColorsMaximizationModule.colorsName[_primaryColor].Substring(0, 1);
-			textMesh.color = Color.white - _primaryColor + Color.black;
+			textMesh.color = negative(_primaryColor);
 		}
+		if (selected) highlight();
 		glass.material.SetColor("_Color", _active ? _primaryColor : Color.black);
+	}
+
+	private void highlight() {
+		Color color = _active && _primaryColor != Color.white ? negative(_primaryColor) : Color.red;
+		button.material.SetColor("_Color", color);
+	}
+
+	private Color negative(Color color) {
+		return Color.white - color + Color.black;
 	}
 }
